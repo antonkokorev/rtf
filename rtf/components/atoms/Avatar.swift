34c9a3fc869073e50524c1b24621b33211ea34b1
antonkokorev <antonkokorev@gmail.com>
2020-01-09 13:20:10 +0300

    avatar

--- rtf/components/atoms/Avatar.swift
+++ rtf/components/atoms/Avatar.swift
@@ -9,23 +9,20 @@
 import SwiftUI
 import URLImage
 
-//Создаем набор параметров для объекта
-struct Parameters{
-    var imageUrl: String
-    var avatarLineColor: Color
-    var avatarLineWidth: CGFloat
-}
 
 struct Avatar:View {
-    let parameters: Parameters
+    @State var imageUrl: String
+    @State var avatarLineColor: Color
+    @State var avatarLineWidth: CGFloat
+    
     var body:some View{
-          URLImage( URL(string: "\(parameters.imageUrl)")!){ proxy in
+          URLImage( URL(string: "\(imageUrl)")!){ proxy in
           proxy.image
               .resizable()                     // Make image resizable
         .cornerRadius(50)
               .padding(5)
         .overlay(RoundedRectangle(cornerRadius: 50)
-            .stroke(self.parameters.avatarLineColor, lineWidth: self.parameters.avatarLineWidth))
+            .stroke(self.avatarLineColor, lineWidth: self.avatarLineWidth))
         .cornerRadius(60)
               .clipped()                       // Clip overlaping parts
               
@@ -40,7 +37,7 @@ struct Avatar:View {
 //Проставляем дефолтные параметры в return
 struct Avatar_Previews: PreviewProvider {
     static var previews: some View {
-        Avatar(parameters: Parameters.init(imageUrl: "", avatarLineColor: Color.white, avatarLineWidth: 0))
+        Avatar(imageUrl: "", avatarLineColor: Color.white, avatarLineWidth: 0)
     }
 }
 
