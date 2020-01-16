0bb9d7fbc7974c2e1df5cc35eb7204fb1d1a5168
antonkokorev <antonkokorev@gmail.com>
2020-01-16 17:37:52 +0300

    kav

--- rtf/components/pages/StartPage.swift
+++ rtf/components/pages/StartPage.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 import PartialSheet
 func greetUser(msg:String) {
-
+    
     
     print(msg)
-
+}
 struct StartPage: View {
-
+    
     
     /* reactiveState */
     @ObservedObject var state: UsersRecentState
@@ -37,157 +37,142 @@ struct StartPage: View {
     
     @State private var showFeedBackPage = false
     @State private var longer: Bool = false
-    @State var showProfileMenu: Bool = false
     
     var body: some View {
-
+        
         NavigationView{
-        ScrollView(.vertical, showsIndicators: false){
-            VStack{
-                /** Аватарка и лайк*/
-                HStack{
-                    Button(action: {
-                        //                        withAnimation{
-                        //                            self.showProfileMenu.toggle()
-                        //                        }
-                    }, label: {
+            ScrollView(.vertical, showsIndicators: false){
+                VStack{
+                    /** Аватарка и лайк*/
+                    HStack{
                         CircleImage(imageUrl: getPhoto("Admin_LB"), imageSize: 40)
                             .onAppear(perform: {
                                 self.store.dispatch(usersActions.pendingGetMe)
                             })
-                    }).self.buttonStyle(PlainButtonStyle())
+                        
+                        Spacer()
+                        Like(number: 5)
+                    }.padding(.vertical, 20.0)
                     
-                    if self.showProfileMenu{
-                        ProfileMenu(firstName: "\(self.users.me.sFirstName!)", lastName: "\(self.users.me.sFirstName!)", imageUrl: getPhoto("Admin_LB")
-                        ).onTapGesture {
-                            self.showProfileMenu.toggle()
-                        }
-                        .offset(x: -50, y: 20)
-                    }
-                    
-                    Spacer()
-                    Like(number: 5)
-                }.padding(.vertical, 20.0)
-                    .zIndex(1)
-                
-                /** Привет userName*/
-                HStack{
-                    Text("Привет,\n\(users.me.sFirstName!)")
-                        .font(Font.Typography.sizingFont(font: .bold, size: .H1))
-                    Spacer()
-                }
-                .padding(.bottom, 25)
-                /** Меню выбора списка юзеров*/
-                HStack(spacing: 15){
-                    
-                    Button(
-                        action: {},
-                        label: {Text("Входящие")
-                    }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textDefault);
-                    Button(
-                        action: {},
-                        label: {Text("Запросы")
-                    }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary);
-                    Button(
-                        action: {},
-                        label: {Text("Недавние")
-                    }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary);
-                    Spacer()
-                }.padding(.bottom, 10)
-                
-                /** Карусель с юзерами*/
-                Carousel(test:greetUser , state: store.state.usersRecentSubState)
-                    .padding(.bottom, 30) .padding(.horizontal, -30).onAppear(perform: {
-                        self.store.dispatch(usersRecentActions.pendingGetRecentUsers)
-                    })
-                /** Меню с кнопками Отчет-История-Статистика*/
-                ScrollView(.horizontal, showsIndicators: false){
+                    /** Привет userName*/
                     HStack{
-                        Spacer(minLength: 30)
+                        Text("Привет,\n\(users.me.sFirstName!)")
+                            .font(Font.Typography.sizingFont(font: .bold, size: .H1))
+                        Spacer()
+                    }
+                    .padding(.bottom, 25)
+                    /** Меню выбора списка юзеров*/
+                    HStack(spacing: 15){
                         
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                            Text("Командный отчет")
-                                .padding(10)
-                        }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefault)
-                            .background(Color.RTFPallete.buttonGrayBackground)
-                            .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
-                        
-                        Button(action: {}) {
-                            Text("История")
-                                .padding(10.0)
-                        }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefaultPale)
-                            .background(Color.RTFPallete.buttonGrayBackground)
-                            .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
-                        
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                            Text("Статиситка")
-                                .padding(10)
-                        }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefaultPale)
-                            .background(Color.RTFPallete.buttonGrayBackground)
-                            .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
+                        Button(
+                            action: {},
+                            label: {Text("Входящие")
+                        }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textDefault);
+                        Button(
+                            action: {},
+                            label: {Text("Запросы")
+                        }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary);
+                        Button(
+                            action: {},
+                            label: {Text("Недавние")
+                        }).font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary);
                         Spacer()
                     }.padding(.bottom, 10)
-                }.padding(.bottom, 25)
-                    .padding(.horizontal, -30)
-                
-                /** Меню с ссылками на приложения*/
-                VStack(spacing: 15){
-                    ActionCard(
-                        action: self.goNextPage,
-                        textTitle: "Коллеги",
-                        textBody: "Обратная связь по компетенциям и проф. навыкам",
-                        icon: "home__feedback"
-                    ).sheet(isPresented: $usersModal) {
-        FeedBackPage(store:  self.store).padding(.top ,35)
+                    
+                    /** Карусель с юзерами*/
+                    Carousel(test:greetUser , state: store.state.usersRecentSubState)
+                        .padding(.bottom, 30) .padding(.horizontal, -30).onAppear(perform: {
+                            self.store.dispatch(usersRecentActions.pendingGetRecentUsers)
+                        })
+                    /** Меню с кнопками Отчет-История-Статистика*/
+                    ScrollView(.horizontal, showsIndicators: false){
+                        HStack{
+                            Spacer(minLength: 30)
+                            
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                Text("Командный отчет")
+                                    .padding(10)
+                            }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefault)
+                                .background(Color.RTFPallete.buttonGrayBackground)
+                                .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
+                            
+                            Button(action: {}) {
+                                Text("История")
+                                    .padding(10.0)
+                            }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefaultPale)
+                                .background(Color.RTFPallete.buttonGrayBackground)
+                                .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
+                            
+                            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                                Text("Статиситка")
+                                    .padding(10)
+                            }.font(Font.Typography.sizingFont(font: .semibold, size: .H3)).foregroundColor(Color.RTFPallete.buttonDefaultPale)
+                                .background(Color.RTFPallete.buttonGrayBackground)
+                                .cornerRadius(BasicRadius.max).padding(.trailing, 15.0)
+                            Spacer()
+                        }.padding(.bottom, 10)
+                    }.padding(.bottom, 25)
+                        .padding(.horizontal, -30)
+                    
+                    /** Меню с ссылками на приложения*/
+                    VStack(spacing: 15){
+                        ActionCard(
+                            action: self.goNextPage,
+                            textTitle: "Коллеги",
+                            textBody: "Обратная связь по компетенциям и проф. навыкам",
+                            icon: "home__feedback"
+                        ).sheet(isPresented: $usersModal) {
+                            FeedBackPage(store:  self.store).padding(.top ,35)
+                        }
+                        ActionCard(
+                            action: self.goNextPage,
+                            textTitle: "Встречи",
+                            textBody: "Обратная связь по компетенциям на встречах",
+                            icon: "home__meetings"
+                        )
+                        ActionCard(
+                            action: self.goNextPage,
+                            textTitle: "Достижения",
+                            textBody: "Обратная связь по достижениям и проектам",
+                            icon: "home__projects"
+                        )
+                        ActionCard(
+                            action: self.goNextPage,
+                            textTitle: "Профоценка SberProfi",
+                            textBody: "Оценка уровня профессионального мастерства",
+                            icon: "home__sberprofi"
+                        )
+                    }.padding(.bottom, 30)
+                    
+                    /** Сообщить об ошибке*/
+                    HStack{
+                        Button(
+                            action: {self.showFeedBackPage = true },
+                            label: {Text("Сообщить об ошибке")
+                                .font(Font.Typography.sizingFont(font: .regular, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary)
+                        })
+                        Spacer()
                     }
-                    ActionCard(
-                        action: self.goNextPage,
-                        textTitle: "Встречи",
-                        textBody: "Обратная связь по компетенциям на встречах",
-                        icon: "home__meetings"
-                    )
-                    ActionCard(
-                        action: self.goNextPage,
-                        textTitle: "Достижения",
-                        textBody: "Обратная связь по достижениям и проектам",
-                        icon: "home__projects"
-                    )
-                    ActionCard(
-                        action: self.goNextPage,
-                        textTitle: "Профоценка SberProfi",
-                        textBody: "Оценка уровня профессионального мастерства",
-                        icon: "home__sberprofi"
-                    )
-                }.padding(.bottom, 30)
+                    Spacer(minLength: 50)
+                }.padding(.horizontal, 30)
                 
-                /** Сообщить об ошибке*/
-                HStack{
-                    Button(
-                        action: {self.showFeedBackPage = true },
-                        label: {Text("Сообщить об ошибке")
-                            .font(Font.Typography.sizingFont(font: .regular, size: .H3)).foregroundColor(Color.RTFPallete.textSecondary)
-                    })
-                    Spacer()
-                }
-                Spacer(minLength: 50)
-            }.padding(.horizontal, 30)
-            
-        }
-        .partialSheet(presented: $showFeedBackPage) {
-            FeedBackPage(store: self.store)
-        }.padding(.top, 30)
-            .background(Color.RTFPallete.backgroundDefault)
-            .edgesIgnoringSafeArea(.all)
-            .onAppear(perform: {
-                self.store.dispatch(usersActions.pendingGetMe)
-            })
-        }
+            }
+                //.partialSheet(presented: $showFeedBackPage) {
+                //            FeedBackPage(store: self.store)
+                //        }
+                .padding(.top, 30)
+                .background(Color.RTFPallete.backgroundDefault)
+                .edgesIgnoringSafeArea(.all)
+                .onAppear(perform: {
+                    self.store.dispatch(usersActions.pendingGetMe)
+                })
         }
     }
 }
 
+
 struct StartPage_Previews: PreviewProvider {
-	static var previews: some View {
-		StartPage(store: AppMain().store)
-	}
+    static var previews: some View {
+        StartPage(store: AppMain().store)
+    }
 }
