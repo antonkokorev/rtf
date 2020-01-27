4cdf220def17d641e63c0f72891d25f6c4762e41
antonkokorev <antonkokorev@gmail.com>
2020-01-27 16:49:28 +0300

    avk

--- rtf/components/molecules/HistoryCards/HistoryAssessmentCard.swift
+++ rtf/components/molecules/HistoryCards/HistoryAssessmentCard.swift
@@ -8,4 +8,28 @@
 
 import SwiftUI
 
+struct HistoryAssessmentCard: View {
+    
+    var imageUrl: String = ""
+    var firstName: String = ""
+    var lastName: String = ""
+    var date: String = ""
+    
+
+    var body: some View {
+        
+        VStack{
+            HStack{
+                Text("Hello world")
+            }
+            Text("Hello world")
+        }
+    }
+}
+
+struct HistoryAssessmentCard_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryAssessmentCard()
+    }
+}
 
