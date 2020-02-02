f7a0501b5340c4b644a286e2134d64c77e84a27f
antonkokorev <antonkokorev@gmail.com>
2020-02-02 19:34:17 +0300

    avk

--- rtf/components/pages/FeedBackRequestPage.swift
+++ rtf/components/pages/FeedBackRequestPage.swift
@@ -9,76 +9,70 @@
 import SwiftUI
 
 struct FeedBackRequestPage: View {
-	
-	@State var searchTxt: String = ""
-	@State var showPopup: Bool = false
-	@State var totalSelected: Int = 0
-	
-	@ObservedObject var competenciesState: FeedbackPropsState = ObservableState(store: mainStore).state.feedbackPropsSubState
-	
-	@ObservedObject var store = ObservableState(store: mainStore)
-	
-	
-//	init(){
-//		self.competenciesState = self.store.state.feedbackPropsSubState
-//	}
-	
-	var body: some View {
-		VStack(alignment: .leading) {
-			Text("Запросить обратную связть")
-				.foregroundColor(Color(red:0.00, green:0.00, blue:0.00))
-				.font(.custom("SBSansDisplay-Regular", size: 28))
-				.fontWeight(.bold)
-				.frame(width: 300)
-				.padding()
-			
-			/** SearchBar **/
-			SearchBar(searchTxt: self.$searchTxt)
-			
-			VStack(alignment: .leading) {
-				
-				//				Text("Системное мышление и решение проблем")
-				//					.foregroundColor(Color(red:0.00, green:0.00, blue:0.00))
-				//					.font(.custom("SBSansDisplay-Regular", size: 20))
-				//					.fontWeight(.bold)
-				//					.frame(width: 350, height: 60)
-				
-				// TODO list inside list bug
-				//				List {
-				ForEach(self.competenciesState.competentions, id: \.self) { item in
-					VStack(alignment: .leading){
-						List {
-							Text(item.sName)
-								.foregroundColor(Color(red:0.00, green:0.00, blue:0.00))
-								.font(.custom("SBSansDisplay-Regular", size: 20))
-								.fontWeight(.bold)
-								.frame(width: 350, height: 60)
-							
-							ForEach(item.aAttributes!, id: \.self) { itemTwo in
-								
-								Competency(showPopup: self.$showPopup, total: self.$totalSelected ,txt: itemTwo.sName)
-							}
-						}
-						
-					}
-				}
-					//				}
-					.partialSheet(presented: self.$showPopup, enableCover: false) {
-						CompetencyRequestPopup(total: self.$totalSelected)
-				}
-				
-			}.padding()
-			
-			
-		}.onAppear(perform: { 
-			self.store.dispatch(feedbackPropsActions.pendingGetAllCompetences)
-		})
-		
-	}
+    
+    @State var searchTxt: String = ""
+    @State var showPopup: Bool = false
+    @State var totalSelected: Int = 0
+    
+    @ObservedObject var competenciesState: FeedbackPropsState = ObservableState(store: mainStore).state.feedbackPropsSubState
+    
+    @ObservedObject var store = ObservableState(store: mainStore)
+    
+    
+    //	init(){
+    //		self.competenciesState = self.store.state.feedbackPropsSubState
+    //	}
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+            VStack(alignment:.leading){
+                Text("Запросить обратную связь")
+                    .font(Font.Typography.sizingFont(font: .bold, size: .H1))
+                    .padding(.bottom, 10)
+                    .padding(.horizontal, 30)
+            }
+            .padding(.top, 40)
+            .padding(.bottom, 20)
+            
+            /** Поиск  **/
+            SearchBar(searchTxt: self.$searchTxt)
+                .padding(.bottom, 20)
+                .padding(.horizontal, 30)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                List {
+                    ForEach(self.competenciesState.competentions, id: \.self) { item in
+                        VStack(alignment: .leading, spacing: 0){
+                            Text(item.sName)
+                                .font(Font.Typography.sizingFont(font: .semibold, size: .H2))
+                            
+                            ForEach(item.aAttributes!, id: \.self) { itemTwo in
+                                Competency(
+                                    showPopup: self.$showPopup,
+                                    total: self.$totalSelected ,
+                                    txt: itemTwo.sName)
+                            }
+                        }
+                        .padding(.bottom, 10)
+                    }
+                }
+                .onAppear {
+                    UITableView.appearance().separatorStyle = .none
+                }
+                .padding(.horizontal, 10)
+                .partialSheet(presented: self.$showPopup, enableCover: false) {
+                    CompetencyRequestPopup(total: self.$totalSelected)
+                }
+            }
+        }.onAppear(perform: {
+            self.store.dispatch(feedbackPropsActions.pendingGetAllCompetences)
+        })
+    }
 }
 
 struct FeedBackRequestPage_Previews: PreviewProvider {
-	static var previews: some View {
-		FeedBackRequestPage()
-	}
+    static var previews: some View {
+        FeedBackRequestPage()
+    }
 }
